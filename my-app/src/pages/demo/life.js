3765a08d8a06f9829e4bca803cb217ee90d5c738
sungy <503391278@qq.com>
2019-12-26 20:28:15 +0800

    update react

---
+++ my-app/src/pages/demo/life.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class Life extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 0
+    }
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+  handleClick1() {
+    this.setState({
+      msg: this.state.msg + 1
+    })
+  }
+  handleClick2 = () => {
+    console.log(this);
+    this.setState({
+      msg: this.state.msg + 1
+    })
+  }
+  handleClick3() {
+    console.log(this);
+    this.setState({
+      msg: this.state.msg + 1
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello world!</h1>
+        <h3>{this.state.msg}</h3>
+        <button onClick={this.handleClick1.bind(this)}>click1</button>
+        <button onClick={this.handleClick2}>click2</button>
+        <button onClick={this.handleClick3}>click3</button>
+      </div>
+    )
+  }
+}
