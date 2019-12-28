41f2e93e8aae091de34d2e14278d73d9b9d07687
sungy <503391278@qq.com>
2019-12-28 14:34:49 +0800

    antd按需引入

--- my-app/src/pages/demo/life.js
+++ my-app/src/pages/demo/life.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './life.less';
+import {Button} from 'antd';
 
 export default class Life extends React.Component {
   constructor(props) {
@@ -34,6 +35,7 @@ export default class Life extends React.Component {
         <button onClick={this.handleClick1.bind(this)}>click1</button>
         <button onClick={this.handleClick2}>click2</button>
         <button onClick={this.handleClick3}>click3</button>
+        <Button>click me!</Button>
       </div>
     )
   }
