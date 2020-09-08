05a4ec39b38b12908d72cb00da28531223a548d0
sunguoyuan <503391278@qq.com>
2020-09-08 22:34:32 +0800

    updae

---
+++ cms/cms-server/docs/user.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (1, 'vv', '123456', 'zhangsan@126.com', '13812341234', 1);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (2, '李四', '123456', 'lisi@126.com', '13912341234', 0);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (3, 'sungy', '123456', 'sungy@126.com', '13512341234', 1);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (5, 'sungy', '123456', 'sungy@126.com', '13512341234', 1);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (6, 'sungy', '123456', 'sungy@126.com', '13512341234', 1);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (7, 'sungy', '123456', 'sungy@126.com', '13512341234', 1);
+INSERT INTO `user`(`id`, `username`, `password`, `email`, `phone`, `gender`) VALUES (8, '阿斯蒂芬', '123456', 'sungy@126.com', '13512341234', 1);
