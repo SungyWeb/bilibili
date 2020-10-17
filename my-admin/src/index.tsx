6a734ab098e3d432db5a10b60132477c7acc2f3d
sunguoyuan <503391278@qq.com>
2020-10-17 09:44:49 +0800

    update

--- my-admin/src/index.tsx
+++ my-admin/src/index.tsx
@@ -2,6 +2,10 @@ import React, { createContext } from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import Page from './Page'
+import './style/index.less'
+import './style/antd/index.less'
+import './style/lib/animate.css'
+
 const GlobalContext = createContext({})
 
 ReactDOM.render(
